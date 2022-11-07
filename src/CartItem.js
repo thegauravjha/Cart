23ef82a7d9971d8996fd2bd2bfb8a8ff9462f92d
Gaurav Jha <gaurav.jhaji20@gmail.com>
2022-11-07 17:17:28 +0530

    Add buttons and Created this.State/constructor

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,32 @@
 import React from "react";
 
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: 'Mobile Phone',
+            price: 999,
+            qty: 1,
+            img: ''
+        }
+    }
+
     render() {
+        const { title, price, qty } = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{ fontSize: 25 }}>Phone</div>
-                    <div style={{ color: '#777' }}>>Rs 999</div>
-                    <div style={{ color: ' #777' }}>>Qty: 1</div>
+                    <div style={{ fontSize: 25 }}>{title}</div>
+                    <div style={{ color: '#777' }}>Rs {price}</div>
+                    <div style={{ color: ' #777' }}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" />
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" />
+                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/484/484662.png" />
                     </div>
                 </div>
             </div>
