a933b94c2e47c058516d77b000e9b329ffce10e1
Gaurav Jha <gaurav.jhaji20@gmail.com>
2022-11-07 17:33:20 +0530

    Add increaseQuantity function [bind/arrow]

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,7 +9,12 @@ class CartItem extends React.Component {
             qty: 1,
             img: ''
         }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
     }
+
+    increaseQuantity = () => {
+        console.log("this", this.state);
+    };
 
     render() {
         const { title, price, qty } = this.state;
@@ -24,9 +29,20 @@ class CartItem extends React.Component {
                     <div style={{ color: ' #777' }}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
-                        <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" />
-                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" />
-                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/484/484662.png" />
+                        <img
+                            alt="increase"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+                            onClick={this.increaseQuantity}
+                        />
+                        <img
+                            alt="decrease"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/512/992/992683.png" />
+                        <img
+                            alt="delete"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/512/484/484662.png" />
                     </div>
                 </div>
             </div>
